5fc2d5c2cf631923bbad83d23c7ba04fa0b504d8
Kate Bugrova <bugrovaev@mail.ru>
2023-08-17 22:23:57 +0300

    ???

--- src/main/java/com/example/tablefx/HelloController.java
+++ src/main/java/com/example/tablefx/HelloController.java
@@ -11,9 +11,13 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
+import static java.nio.file.Files.readAllLines;
 
 public class HelloController {
     @FXML
@@ -21,56 +25,45 @@ public class HelloController {
     @FXML
     Button button;
 
-    public  List<String> read() throws FileNotFoundException {
-        String a=null;
-        String b=null;
-        String c =null;
-        int line=0;
-        File file=new File("1.txt");
-        Scanner scanner=new Scanner(file);
-        List<String> list = List.of(scanner.next().split(" "));
-        while (scanner.hasNext()){
-            list.add(scanner.next());
-            a=list.get(0);
-            b=list.get(1);
-            c=list.get(2);
-            line++;
+    public void initialize()
+    {
+        try {
+            makeTable(read("1.txt"));
+        } catch (IOException e) {
+            System.out.println(":((");
         }
-        ObservableList<Triangle> triangleObservableList = FXCollections.observableArrayList(
-                new Triangle(a,b,c),
-                new Triangle(a,b,c),
-                new Triangle(a,b,c)
-        );
-        TableView<Triangle> tableView=new TableView<>(triangleObservableList);
-        TableColumn<Triangle, String> aColumn=new TableColumn<>("A");
-        aColumn.setCellValueFactory(new PropertyValueFactory<Triangle,String>("a"));
-        tableView.getColumns().add(aColumn);
-        TableColumn<Triangle, String> bColumn=new TableColumn<>("B");
-        bColumn.setCellValueFactory(new PropertyValueFactory<Triangle,String>("b"));
-        tableView.getColumns().add(bColumn);
-        TableColumn<Triangle, String> cColumn=new TableColumn<>("C");
-        cColumn.setCellValueFactory(new PropertyValueFactory<Triangle,String>("c"));
-        tableView.getColumns().add(cColumn);
-
-        return list;
     }
-    public void doPloshad() throws FileNotFoundException {
-        int a,b,c;
-        int area,pl;
-        File file=new File("1.txt");
+    public ObservableList<Triangle> read(String filename) throws IOException {
+        File file=new File(filename);
         Scanner scanner=new Scanner(file);
-        List<String> list = new ArrayList<>();
+        ObservableList<Triangle> triangleObservableList = FXCollections.observableArrayList();
+        String a,b,c;
         while (scanner.hasNext()){
-            list.add(scanner.next());
+            a = scanner.next();
+            b= scanner.next();
+            c= scanner.next();
+            triangleObservableList.add(new Triangle(a,b,c));
         }
-        String[] array = list.toArray(new String[0]);
-        for (int i = 0; i < array.length; i=i+3) {
-            a= Integer.parseInt(array[i]);
-            b= Integer.parseInt(array[i+1]);
-            c= Integer.parseInt(array[i+2]);
-            pl=(a+b+c)/2;
-            area= (int) Math.sqrt(pl*(pl-a)*(pl-b)*(pl-c));
-            }
+        return triangleObservableList;
+
+
+    }
+
+    public void makeTable(ObservableList<Triangle> triangleObservableList)
+    {
+
+        TableColumn<Triangle, Double> aColumn=new TableColumn<>("C");
+        aColumn.setCellValueFactory(new PropertyValueFactory<>("x"));
+        tableView.getColumns().add(aColumn);
+        TableColumn<Triangle, Double> bColumn=new TableColumn<>("B");
+        bColumn.setCellValueFactory(new PropertyValueFactory<>("y"));
+        tableView.getColumns().add(bColumn);
+        TableColumn<Triangle, Double> cColumn=new TableColumn<>("C");
+        cColumn.setCellValueFactory(new PropertyValueFactory<>("z"));
+        tableView.getColumns().add(cColumn);
+        TableColumn<Triangle, Double> columnArea = new TableColumn<>("Area");
+        columnArea.setCellValueFactory(new PropertyValueFactory<>("area"));
+        tableView.setItems(triangleObservableList);
     }
 
 }
--- src/main/java/com/example/tablefx/Triangle.java
+++ src/main/java/com/example/tablefx/Triangle.java
@@ -1,34 +1,71 @@
 package com.example.tablefx;
 
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class Triangle {
-    SimpleStringProperty x,y,z;
+    SimpleDoubleProperty x,y,z;
+    private SimpleDoubleProperty perimeter = new SimpleDoubleProperty();
+    private SimpleDoubleProperty area = new SimpleDoubleProperty();
 
-    public Triangle(String x, String y, String z) {
-        this.x = new SimpleStringProperty(x);
-        this.y = new SimpleStringProperty(y);
-        this.z = new SimpleStringProperty(z);
+
+
+    public Triangle(double x, double y, double z) {
+        this.x = new SimpleDoubleProperty(x);
+        this.y = new SimpleDoubleProperty(y);
+        this.z = new SimpleDoubleProperty(z);
+
+        {//секция инициализации
+            this.x.addListener((s1, o,n)->{
+                this.perimeter.set(perimeter());
+                this.area.set(area());
+            });
+            this.y.addListener((s1, o,n)->{
+                this.perimeter.set(perimeter());
+                this.area.set(area());
+            });
+            this.z.addListener((s1, o,n)->{
+                this.perimeter.set(perimeter());
+                this.area.set(area());
+            });
+        }
+
     }
 
-    public String getX() {
+    public double getX() {
         return x.get();
     }
 
-    public void setX(String value) {
+    public void setX(double value) {
         x.set(value);
     }
 
-    public String getY() {
+    public double getY() {
         return y.get();
     }
-    public void setY(String value) {
+    public void setY(double value) {
         y.set(value);
     }
-    public String getZ() {
+    public double getZ() {
         return z.get();
     }
-    public void setC(String value) {
+    public void setC(double value) {
         z.set(value);
+    }
+
+    public Triangle(String x, String y, String z) {
+        this.x.set(Double.parseDouble(x));
+        this.y.set(Double.parseDouble(y));
+        this.z.set(Double.parseDouble(z));
+    }
+
+    double perimeter()
+    {
+        return x.get()+y.get()+z.get();
+    }
+    double area()
+    {
+        double p = perimeter.get()/2;
+        return Math.sqrt(p*(p-x.get())*(p-y.get())*(p-z.get()));
     }
 }
